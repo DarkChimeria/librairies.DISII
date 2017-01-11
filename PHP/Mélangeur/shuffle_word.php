d57192a6376c062d1479518c965b00058a73de49
Anthony S <dissident2834@gmail.com>
2017-01-11 08:17:51 +0100

    Add Shuffle Word
    Add shuffle_word.php

---
+++ PHP/Mélangeur/shuffle_word.php
@@ -0,0 +1,19 @@
+ <?php
+
+// Mélange un mot
+function melange( $m )
+{
+  $s = $m[0];
+  $n = strlen($s);
+
+  // Moins de 4 lettres : pas de mélange à faire
+  if( $n < 4 )
+    return $s;
+
+  return $s[0] . str_shuffle( substr($s, 1, $n-2) ) . $s[$n-1];
+}
+
+$txtIn  = stripslashes( @$_REQUEST['texte'] );
+$txtOut = preg_replace_callback('/\p{L}+/', 'melange', $txtIn );
+
+?> 
